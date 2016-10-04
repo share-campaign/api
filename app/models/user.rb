903901425c89cf135089f1396da0a57294f2ca62
Eksavoy <qTVDuCaN#7LOoEeg>
2016-10-04 10:27:22 +0200

    Add roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
+  before_save :init
+
+  ROLES = %i[user moderator admin]
+
+  def has_role?(role)
+    self.roles == role
+  end
+
+  private
+
+  def init
+    self.roles ||= 'user'
+  end
 end
